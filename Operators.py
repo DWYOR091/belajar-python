b982dc67c5d6da90d10fe14b46ba95e8e84e6a94
M Rifqi <muhamadrifqi.id@gmail.com>
2025-03-25 16:46:45 +0700

    operators

---
+++ Operators.py
@@ -0,0 +1,46 @@
+#Arithmetic Operators
+a = 10
+b = 2
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)
+print(a//b) #pembagian bulat
+
+#Assignment Operators
+a += b
+print(a)
+
+a -= b 
+print(a)
+a *= b
+print(a)
+a /= b
+print(a)
+a **= b
+print(a)
+a //= 2
+print(a)
+a //= 2
+print(a)
+a %= 2
+print(a)
+# print(tes1 = "kocak") #error
+print(tes2 := "kocak") #kocak
+
+#logical operator  and,or,not
+angka1 = 3
+angka2 = 1
+
+if not angka1 > 5 and angka2 < 5:
+    print("betul")
+else:
+    print("salah")
+
+#compare operator
+print(angka1 > angka2)
+print(angka1 < angka2)
+print(angka1 == angka2)
+print(angka1 != angka2)
