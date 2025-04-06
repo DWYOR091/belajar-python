10d8b357baa30bd0a2a16bfdefb3591bd6467487
M Rifqi <muhamadrifqi.id@gmail.com>
2025-04-06 10:27:47 +0700

    if eles

---
+++ if-else/if-else.py
@@ -0,0 +1,20 @@
+#greater than
+a = 50
+b = 30
+
+if a > b:
+    print("betul")
+elif a == b:
+    print("sama")
+else:
+    print("salah")
+    
+#shorthand
+if a > b: print("betul")
+
+print("betul") if a > b else print("salah")
+
+#pass
+if a > b :
+    pass
+
