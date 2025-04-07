1c774eba6ed0c4701a8b63b7106ffc1da9524331
M Rifqi <muhamadrifqi.id@gmail.com>
2025-04-07 19:27:11 +0700

    python math

---
+++ PythonMath.py
@@ -0,0 +1,34 @@
+#import math
+import math
+
+#min dan max
+a = min(12,2,34,21,3,1)
+b = max(12,2,34,21,3,1)
+print(a)
+print(b)
+
+#abs positif nilai
+c = abs(-3.42)
+print(c)
+
+#pow pangkat
+d = pow(2,3)
+print(d)
+print(2**3)
+
+e = math.pow(2,3)
+print(e)
+
+#pembulatan kebawah
+f = math.floor(3.7)
+print(f)
+
+#pembulatan keatas
+g = math.ceil(3.7)
+print(g)
+# pembulatan atas atau bawah
+h = round(3.7)
+print(h)
+# pi 3.14
+i = math.pi
+print(i)
